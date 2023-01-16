049e1a060270cd68b4c4caea87e9b3f2e081df0b
Luis Zelaya <codewithluis11@gmail.com>
2023-01-16 16:50:13 -0600

    add product model, migration and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             // EmployeeSeeder::class,
-            UsersSeeder::class
+            UsersSeeder::class,
+            ProductSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
